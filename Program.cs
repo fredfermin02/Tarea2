32d917e065eb30d748cee77770e9b6d122adbeea
WilmerCM01 <wilmer.caraballo09@gmail.com>
2021-05-14 02:22:12 -0400

    Prueba2

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 
-namespace Persistenciamultiplicatica
+namespace Programas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            
+            Console.Clear();
+
+            Console.WriteLine("Introduzca un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El primer digito de ese numero es: " + num);
+
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para finalizar...");
+            Console.ReadKey();
 
         }
+
     }
 }
